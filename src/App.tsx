6e8a324a638c6f5155ff30b7af7e32f83664e79d
Aaditya Nahar Jain <aadityanaharjain2527@gmail.com>
2026-01-04 19:23:24 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -40,8 +40,8 @@ export default function Page() {
           <>
             <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
             <div className="text-container">Ok yay!!!</div>
-            <a href="https://willyoubemyvalentinevk.vercel.app/" target="_blank"></a>
-            
+            <a href="https://willyoubemyvalentinevk.vercel.app/" target="_blank">
+            <img src="https://www.bing.com/ck/a?!&&p=c08737c28e090f60fe947132794e9682d32c9fd96f2f445ac8412e2a5aac046fJmltdHM9MTc2NzQ4NDgwMA&ptn=3&ver=2&hsh=4&fclid=0a4c7080-1a65-673e-3351-66bc1b0f6640&u=a1L2ltYWdlcy9zZWFyY2g_cT13aGl0ZStpbWFnZSZpZD1GN0JEODRGMzg0OTk2NEJCNzMyOTlGQjQxMjdFNThEOEVFNUVCQkM0JkZPUk09SUFDRklS&ntb=1" alt="Clickable Image" style="width:300px;height:auto;"></img>
           </>
         ) : (
           <>
@@ -70,3 +70,4 @@ export default function Page() {
     </div>
   );
 }
+  
